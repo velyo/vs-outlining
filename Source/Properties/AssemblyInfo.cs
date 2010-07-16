b7c34347535005e2e0bfc68833fc56ef63daddac
velio <none@none>
2010-07-16 06:24:08 +0000

    Release 1.2 with updated and proper assembly version and package informatoin.

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -4,14 +4,11 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("JsOutlining")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("JavaScript & CSS Outlining")]
+[assembly: AssemblyDescription("Visual Studio 2010 editor extension for JavaScript and CSS code blocks and custom regions outlining")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Artem")]
-[assembly: AssemblyProduct("JsOutlining")]
+[assembly: AssemblyCompany("ArtemBG - http://artembg.com")]
+[assembly: AssemblyProduct("Visual Studio 2010 Outlining")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]   
@@ -19,18 +16,8 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.10196.715")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.10196.715")]
 
 
 
